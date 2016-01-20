16c7de5c59b7719cdf2cd28c1b4e0bd68326abd1
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-20 10:35:32 -0500

    change bg color on console log

--- js/console.js
+++ js/console.js
@@ -1,2 +1,2 @@
 console.info("%cThanks for Visiting the NCSS website, now go EAT there!","background-color:orange");
-console.info("Please check out our facebook page for up to date specials and holiday hours");
+console.info("%cPlease check out our facebook page for up to date specials and holiday hours","background-color:yellow");
