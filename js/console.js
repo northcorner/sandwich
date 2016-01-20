acfecb1e0632171bb55077325782c00f08462872
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-20 10:23:51 -0500

    add console info correction!

--- js/console.js
+++ js/console.js
@@ -1,1 +1,2 @@
-console.info("c%Thanks for Visiting the NCSS website, now go EAT there!","background-color:orange");
+console.info("%cThanks for Visiting the NCSS website, now go EAT there!","background-color:orange");
+console.info("Please check out our facebook page for up to date specials and holiday hours");
